dd51f256a7eca8b272a16d7735473bd554d8da29
Max Salnikov <liss38@yandex.ru>
2018-02-01 17:55:02 +0300

    clear space

--- source/pages/blog/blog.js
+++
@@ -1,8 +0,0 @@
-import 'normalize.css';
-import './blog.scss';
-
-import createMenu from '../../components/menu/menu';
-const menu = createMenu(['Главная', 'Блог'], 'menu');
-document.body.appendChild(menu);
-
-console.log('in blog.js');
